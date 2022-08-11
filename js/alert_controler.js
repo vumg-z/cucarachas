ae2487b381175c1c217a6eb331ec099d357b30d9
um <ab17100222@gmail.com>
2022-08-11 16:35:09 -0500

    messages for win and lose conditions

--- js/alert_controler.js
+++ js/alert_controler.js
@@ -3,11 +3,16 @@ function alertControler(message, isVisible, isWin){
     let alertText = document.getElementById("alert-text")
     let alertImg = document.getElementById("alert-img")
 
+    let messages_win = ["You win", "You are awesome!", "Fantastic", "OH GOD!", "No way Jose!!!", "Verlo"]
+    let messages_fail = ["HORRIBLE", "Please quit", "Trash", "You can do it better", "Really?", "Not even close!"]
+
     if(isWin){
         alertImg.src = "./assets/splash.png"
+        message = messages_win[Math.floor(Math.random()*messages_win.length)]
     }else{
         //cambia la ruta a la cucaracha muerta
         alertImg.src = "./assets/muerto.png"
+        message = messages_fail[Math.floor(Math.random()*messages_fail.length)]
     }
 
     if(isVisible){
--- js/messages_data.js
+++ js/messages_data.js
@@ -1,1 +1,2 @@
-messages = ["You win", "You are awesome!", "Fantastic", "OH GOD!", "No way Jose!!!", "Verlo"]
+messages_win = ["You win", "You are awesome!", "Fantastic", "OH GOD!", "No way Jose!!!", "Verlo"]
+messages_fail = ["HORRIBLE", "Please quit", "Trash", "You can do it better", "Really?", "Not even close!"]
